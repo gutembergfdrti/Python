3636fd3d0ee19137cbe005c0b8d06aa602186b99
gutembergfdrti <gutembergferreirati@outlook.com>
2021-11-27 12:29:13 -0300

    Código Sintaxe e Input

---
+++ Curso Python Mundo 1/sintaxe_inicial.py
@@ -0,0 +1,16 @@
+print('Olá mundo!') # String simples, escrever caracteres na Tela. Resultado Olá mundo!
+
+print('7'+'5') # Exemplo de união de duas strings ou números. Resultado 75
+
+print(7+4) # Exemplo de cálculo matemático adição. Resultado 11
+
+nome = 'Gutemberg'
+idade = 25
+peso = 62
+
+print(nome, idade, peso)
+
+
+
+
+
